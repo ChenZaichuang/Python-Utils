288830ad7c14da4dcf80ae2677e4ec83f6b806f5
Chen Zaichuang <1158400745@qq.com>
2020-04-17 00:49:46 +0800

    fix SCRIPT_PATH value

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-SCRIPT_PATH=$(pwd)
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pip3 install -r ${SCRIPT_PATH}/requirements.txt
 find ${SCRIPT_PATH} -type d -empty -delete
