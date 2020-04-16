272444e0c10eb80b0e88d7f3ed5bf3005a757c24
Chen Zaichuang <1158400745@qq.com>
2020-04-16 16:22:23 +0800

    Update methods in misc.

--- misc.py
+++ misc.py
@@ -3,24 +3,30 @@ import pickle
 from datetime import timedelta, datetime
 
 
-def get_variable_from_local(variable_name_with_path, **kwargs):
-    assert "default_value" in kwargs or 'function_value' in kwargs
+def get_variable_from_local(variable_name_with_path, default_value=None, function_value=None, args=None, kwds=None, keep_time=None, local_expire_datetime=None):
+    assert default_value or function_value
+    if "pkl_file_path" in os.environ:
+        variable_name_with_path = f'{os.environ["pkl_file_path"]}/{variable_name_with_path}'
     if os.path.exists(f'{variable_name_with_path}.pkl'):
         with open(f'{variable_name_with_path}.pkl', 'rb') as f:
             info_map = pickle.load(f)
-        if "keep_time" in kwargs or "local_expire_datetime" in kwargs:
-            retire_datetime = info_map["created_date"] + timedelta(seconds=int(kwargs["keep_time"])) if "keep_time" in kwargs else datetime.strptime(kwargs["local_expire_datetime"], '%Y-%m-%d %H:%M:%S')
+        if keep_time or local_expire_datetime:
+            retire_datetime = info_map["created_date"] + timedelta(seconds=int(keep_time)) if keep_time else datetime.strptime(local_expire_datetime, '%Y-%m-%d %H:%M:%S')
             current_time = datetime.now()
             if current_time <= retire_datetime:
                 return info_map["variable_value"]
         else:
             return info_map["variable_value"]
-    variable_value = kwargs["default_value"] if "default_value" in kwargs else kwargs["function_value"]()
+    if args is None:
+        args = ()
+    if kwds is None:
+        kwds = {}
+    variable_value = default_value if default_value else function_value(*args, **kwds)
     with open(f'{variable_name_with_path}.pkl', 'wb') as f:
         pickle.dump({"variable_value": variable_value, "created_date": datetime.now()}, f)
     return variable_value
 
 
-def date_range(start_date, end_date):
-    for n in range(int ((end_date - start_date).days)):
+def date_range(start_date, end_date, including_end_date=False):
+    for n in range(int((end_date - start_date).days) + (1 if including_end_date else 0)):
         yield start_date + timedelta(n)
