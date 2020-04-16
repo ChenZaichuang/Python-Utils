c9038caf432cef8c1d0b28e2722d6996a792a7ab
Chen Zaichuang <1158400745@qq.com>
2020-04-16 22:49:30 +0800

    Fix git_repo argument.

--- init_submodule.sh
+++ init_submodule.sh
@@ -1,6 +1,7 @@
 submodule_name=$1
+git_repo=$2
 git rm -r --cached ${submodule_name}
 rm -rf .git/modules/${submodule_name}
 rm -r ${submodule_name}
-git submodule add git@github.com:ChenZaichuang/DNS-Resolver.git ${submodule_name}
+git submodule add ${git_repo} ${submodule_name}
 ./${submodule_name}/init.sh
