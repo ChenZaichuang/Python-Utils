bd217bb3c5880b918871f86abd8c88b0b39bd369
Chen Zaichuang <1158400745@qq.com>
2020-06-14 00:01:10 +0800

    Fix logger duplicate issue.

--- logger.py
+++ logger.py
@@ -1,11 +1,13 @@
-import json
 import logging
-import os
-import sys
 from logging.handlers import TimedRotatingFileHandler
+import sys
 
 
 class CustomLogger:
+    use_global_config = False
+    global_stdout_logger = None
+    global_stderr_logger = None
+
     log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     stdout_stream_handler = logging.StreamHandler(sys.stdout)
     stdout_stream_handler.setFormatter(log_formatter)
@@ -17,51 +19,44 @@ class CustomLogger:
 
     def __init__(self, level=logging.DEBUG, to_console=True, to_file_name='', with_requests_logger=False, time_rotating=None):
 
-        if time_rotating is not None:
-            assert type(time_rotating) is dict and "when" in time_rotating and "interval" in time_rotating
-
-        level = int(os.environ.get('logging_level', level))
-        to_console = str(os.environ.get('logging_to_console', to_console))
-        to_file_name = os.environ.get('logging_to_file_name', to_file_name)
-        with_requests_logger = str(os.environ.get('logging_with_requests_logger', with_requests_logger))
-
-        if 'logging_time_rotating' in os.environ:
-            time_rotating = json.loads(os.environ['logging_time_rotating'])
-
-        if with_requests_logger:
-            self.stdout_logger = logging.getLogger("urllib3")
+        if CustomLogger.use_global_config:
+            self.stdout_logger = CustomLogger.global_stdout_logger
+            self.stderr_logger = CustomLogger.global_stderr_logger
         else:
-            self.stdout_logger = logging.getLogger(f"stdout_logger_{CustomLogger.console_logger_index}")
+            if time_rotating is not None:
+                assert type(time_rotating) is dict and "when" in time_rotating and "interval" in time_rotating
 
-        self.stdout_logger.propagate = False
-        self.stdout_logger.setLevel(level)
-
-        self.stderr_logger = logging.getLogger(f"stderr_logger_{CustomLogger.console_logger_index}")
-        self.stderr_logger.propagate = False
-        self.stderr_logger.setLevel(logging.ERROR)
-
-        if to_console:
-            self.stdout_logger.addHandler(self.stdout_stream_handler)
-            self.stderr_logger.addHandler(self.stderr_stream_handler)
-
-        if to_file_name:
-            if time_rotating is None:
-                file_handler = logging.FileHandler(to_file_name, mode='w')
+            if with_requests_logger:
+                self.stdout_logger = logging.getLogger("urllib3")
             else:
-                file_handler = TimedRotatingFileHandler(filename=to_file_name, when=time_rotating['when'], interval=time_rotating['interval'])
-            file_handler.setFormatter(self.log_formatter)
-            self.stdout_logger.addHandler(file_handler)
-            self.stderr_logger.addHandler(file_handler)
+                self.stdout_logger = logging.getLogger(f"stdout_logger_{CustomLogger.console_logger_index}")
+
+            self.stdout_logger.propagate = False
+            self.stdout_logger.setLevel(level)
+
+            self.stderr_logger = logging.getLogger(f"stderr_logger_{CustomLogger.console_logger_index}")
+            self.stderr_logger.propagate = False
+            self.stderr_logger.setLevel(logging.ERROR)
+
+            if to_console:
+                self.stdout_logger.addHandler(self.stdout_stream_handler)
+                self.stderr_logger.addHandler(self.stderr_stream_handler)
+
+            if to_file_name:
+                if time_rotating is None:
+                    file_handler = logging.FileHandler(to_file_name, mode='w')
+                else:
+                    file_handler = TimedRotatingFileHandler(filename=to_file_name, when=time_rotating['when'], interval=time_rotating['interval'])
+                file_handler.setFormatter(self.log_formatter)
+                self.stdout_logger.addHandler(file_handler)
+                self.stderr_logger.addHandler(file_handler)
 
     @classmethod
     def set_global_config(cls, level=logging.DEBUG, to_console=True, to_file_name='', with_requests_logger=False, time_rotating=None):
-        os.environ['logging_level'] = str(level)
-        os.environ['logging_to_console'] = str(to_console)
-        os.environ['logging_to_file_name'] = to_file_name
-        os.environ['logging_with_requests_logger'] = str(with_requests_logger)
-        if time_rotating is not None:
-            assert type(time_rotating) is dict and "when" in time_rotating and "interval" in time_rotating
-            os.environ['logging_time_rotating'] = json.dumps(time_rotating)
+        logger = CustomLogger(level=level, to_console=to_console, to_file_name=to_file_name, with_requests_logger=with_requests_logger, time_rotating=time_rotating)
+        CustomLogger.global_stdout_logger = logger.stdout_logger
+        CustomLogger.global_stderr_logger = logger.stderr_logger
+        CustomLogger.use_global_config = True
 
     def debug(self, msg):
         self.stdout_logger.debug(msg)
@@ -75,7 +70,7 @@ class CustomLogger:
 
 if __name__ == '__main__':
 
-    CustomLogger.set_global_config(level=logging.INFO, with_requests_logger=True)
+    CustomLogger.set_global_config(level=logging.INFO, to_file_name='test.log', with_requests_logger=True)
 
     logger = CustomLogger(level=logging.DEBUG, to_file_name='test.log', with_requests_logger=True)
     try:
