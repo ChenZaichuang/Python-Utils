f801cc3808a15add4c0dabab22709c671dc01b66
zcchen <zcchen@thoughtworks.com>
2020-09-30 14:13:29 +0800

    Add timeout_retry function.

--- misc.py
+++ misc.py
@@ -1,6 +1,11 @@
 import os
 import pickle
 from datetime import timedelta, datetime
+import functools
+import traceback
+
+from gevent import Timeout, sleep
+from gevent.timeout import Timeout as TimeoutException
 
 
 def get_variable_from_local(variable_name_with_path, default_value=None, function_value=None, args=None, kwds=None, keep_time=None, local_expire_datetime=None, force_refresh=False):
@@ -37,3 +42,30 @@ def list_to_string(a_list):
 
 def set_to_string(a_set):
     return "('" + "','".join(a_set) + "')"
+
+def timeout_retry(original_function=None, timeout=30, retries=5, retry_interval=3):
+
+    def _decorate(function):
+
+        @functools.wraps(function)
+        def wrapped_function(*args, **kwargs):
+            for _ in range(retries):
+                try:
+                    with Timeout(timeout):
+                        res = original_function(*args, **kwargs)
+                        if res is not None:
+                            return res
+                except TimeoutException:
+                    sleep(retry_interval)
+                except Exception:
+                    err_msg = traceback.format_exc()
+                    print(err_msg)
+                    raise RuntimeError(f"Unknow Exception: {err_msg}")
+            raise RuntimeError(f"Query Get None after {retries} times")
+
+        return wrapped_function
+
+    if original_function:
+        return _decorate(original_function)
+
+    return _decorate
